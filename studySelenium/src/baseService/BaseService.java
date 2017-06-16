044230ed7854de42f4497013a00a54cef7f43dc7
shenpp1214 <shenpp@cpsdna.com>
2017-06-16 11:28:15 +0800

    #0616更新脚本

--- studySelenium/src/baseService/BaseService.java
+++ studySelenium/src/baseService/BaseService.java
@@ -7,18 +7,27 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import baseService.poi.SeleniumExcel;
 
 import baseService.PropertiesReader;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 public class BaseService {
 	protected static WebDriver dr;
 	protected static Properties props;
+	protected static SeleniumExcel excel;
 	@Rule
 	public Timeout globalTimeout = new Timeout(10 * 60 * 1000);// 设置超时10分钟
 
@@ -27,7 +36,21 @@ public class BaseService {
 
 		System.setProperty("webdriver.chrome.driver", props.getProperty("driver_path"));
 
-		dr = new ChromeDriver();
+		String downloadFilepath = "E:\temp";
+		Map<String, Object> contentSettings = new HashMap<String, Object>();
+		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+		contentSettings.put("multiple-automatic-downloads", 1);
+		chromePrefs.put("profile.default_content_settings", contentSettings);
+		chromePrefs.put("download.prompt_for_download", "false");
+		chromePrefs.put("browser.download.dir", downloadFilepath);
+
+		// chromePrefs.put("download.prompt_for_download", "false");
+		// chromePrefs.put("profile.default_content_settings.popups", 0);
+		// chromePrefs.put("download.default_directory", downloadFilepath);
+
+		ChromeOptions option = new ChromeOptions();
+		option.setExperimentalOption("prefs", chromePrefs);
+		dr = new ChromeDriver(option);
 		dr.manage().window().maximize();
 		dr.get(props.getProperty(url));
 	}
@@ -82,5 +105,31 @@ public class BaseService {
 	protected String getTemplatePath(String key) {
 		return System.getProperty("user.dir") + System.getProperty("file.separator") + "template"
 				+ System.getProperty("file.separator") + props.getProperty(key);
+	}
+
+	/**
+	 * 
+	 * @param sheetName
+	 *            sheet名字
+	 * @param row
+	 *            行号
+	 * @param column
+	 *            列号
+	 * @return
+	 * @throws Exception
+	 */
+	protected static String getCellValue(String sheetName, int row, int column) throws Exception {
+		String templateDir = System.getProperty("user.dir") + System.getProperty("file.separator") + "temp"
+				+ System.getProperty("file.separator");
+		File[] files = new File(templateDir).listFiles();
+		Arrays.sort(files, new Comparator<File>() {
+			@Override
+			public int compare(File file1, File file2) {
+				return (int) (file2.lastModified() - file1.lastModified());
+			}
+		});
+
+		excel = new SeleniumExcel(files[0], sheetName);
+		return excel.getCellValue(row - 1, column - 1);
 	}
 }
